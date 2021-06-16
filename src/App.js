9605f9365078520823c355c8919c4c9c83bd11b5
apJammanbo <apJammanbo@gmail.com>
2021-06-16 16:50:02 +0900

    sns buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, Button, VStack } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <VStack m={10}>
+        <Button colorScheme="green" size="lg">
+          Naver
+        </Button>
+        <Button colorScheme="yellow" size="lg">
+          Kakao
+        </Button>
+        <Button colorScheme="facebook" size="lg">
+          FaceBook
+        </Button>
+        <Button colorScheme="twitter" size="lg">
+          Twitter
+        </Button>
+      </VStack>
+    </ChakraProvider>
   );
 }
 
